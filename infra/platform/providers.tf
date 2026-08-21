57700f03b71e5efafc2b28c8bff2344c00489b31
chandsharma_microsoft <chandsharma@microsoft.com>
2026-08-21 07:41:05 +0530

    fix

--- infra/platform/providers.tf
+++ infra/platform/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.14.0, < 5.1.1"
+      version = ">= 4.14.0, < 5.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
